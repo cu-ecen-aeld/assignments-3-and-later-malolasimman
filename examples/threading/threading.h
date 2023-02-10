61f3931fe562049d8f034391bd3f39c756ced85c
malolasimman <malolasimman7101999@gmail.com>
2023-02-10 04:47:37 +0000

    passes unittest localy

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,7 +14,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
-
+     pthread_mutex_t *lock;
+     int wait_to_obtain;
+     int wait_to_release;
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
